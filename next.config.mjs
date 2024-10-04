c1f7669cfdbab7bfa99843f67249d0b91cd9ec74
Mac_codeplay <tidtaphon.s@code-play.net>
2024-10-04 16:26:03 +0700

    Set image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'profile.line-scdn.net',
+            port: '',
+            pathname: '/*',
+            },
+            
+        ],
+      },
+};
 
 export default nextConfig;
